65dd89a1b6634349f987081eae4fe076f1514b2e
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-03-02 13:44:49 +0530

    Added Reduction Constructs

--- src/real_e.C
+++ src/real_e.C
@@ -4,7 +4,7 @@
 #define NUM_THREADS 10
 #define PAD 8
 
-/* false sharing */
+//* false sharing and padding
 // double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
 //     int nthreads;
 //     double sum[NUM_THREADS][PAD],real_energy=0;
@@ -32,34 +32,36 @@
 //     return real_energy;
 // }
 
-/* synchromization construct critical */
-double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
-    int nthreads;
-    double real_energy=0;
-    omp_set_num_threads(NUM_THREADS);
+//* synchromization construct critical
+// double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
+//     int nthreads;
+//     double real_energy=0;
+//     omp_set_num_threads(NUM_THREADS);
 
-    #pragma omp parallel
-    {
-        int i, id, nthrds;
-        double sum;
-        id = omp_get_thread_num();
-        nthrds = omp_get_num_threads();
-        if(id==0) nthreads=nthrds;
-        for (i = id,sum=0; i < natoms; i+=nthrds){
-            for (int j = 0; j < i; j++){
-                if(i!=j){
-                    double modR=dist(PosIons,i,j,box);
-                    sum+=(ion_charges[i]*ion_charges[j]*erfc(betaa*modR))/modR;
-                }
-            }
-        }
-        #pragma omp critical
-            real_energy+=sum;
-    }
-    return real_energy;
-}
+//     #pragma omp parallel
+//     {
+//         int i, id, nthrds;
+//         double sum;
 
-/*Original loop, no parallelization*/
+//         id = omp_get_thread_num();
+//         nthrds = omp_get_num_threads();
+//         if(id==0) nthreads=nthrds;
+
+//         for (i = id,sum=0; i < natoms; i+=nthrds){
+//             for (int j = 0; j < i; j++){
+//                 if(i!=j){
+//                     double modR=dist(PosIons,i,j,box); // calculating the minimum distance between the given i and j atoms
+//                     sum+=(ion_charges[i]*ion_charges[j]*erfc(betaa*modR))/modR;
+//                 }
+//             }
+//         }
+//         #pragma omp critical // this section stops the all other threads and first performs this action
+//             real_energy+=sum;
+//     }
+//     return real_energy;
+// }
+
+//*Original loop, no parallelization
 // double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
 //     double real_energy=0;
 //     for (int i = 0; i < natoms; i++){
@@ -73,3 +75,19 @@ double real_energy(double **PosIons, float *ion_charges, int natoms, double beta
     
 //     return real_energy;
 // }
+
+//* For reduction construct
+double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
+    double real_energy=0;
+    #pragma omp parallel for reduction(+: real_energy)
+        for (int i = 0; i < natoms; i++){
+            for (int j = 0; j < i; j++){
+                if(i!=j){
+                    double modR=dist(PosIons,i,j,box);
+                    real_energy+=(ion_charges[i]*ion_charges[j]*erfc(betaa*modR))/modR;
+                }
+            }
+        }
+    
+    return real_energy;
+}
--- src/reciprocal.C
+++ src/reciprocal.C
@@ -4,7 +4,7 @@
 #include "omp.h"
 #define NUM_THREADS 10
 
-/*Original loop, no parallelization*/
+//*Original loop, no parallelization
 // double reci_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K){
 //     double reci_energy=0;
 //     for (int kx = -K; kx < K+1; kx++){
@@ -29,7 +29,34 @@
 //     return reci_energy;
 // }
 
-/* synchromization construct critical */
+//* For reduction construct */
+/*There is no workaround for the complex reduction, we have to do the ugly work*/
+// double reci_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K){
+//     double reci_energy=0;
+//     for (int kx = -K; kx < K+1; kx++){
+//         for (int ky = -K; ky < K+1; ky++){
+//             for (int kz = -K; kz < K+1; kz++){
+//                 if((kx==0) && (ky==0) && (kz==0))continue;
+//                 complex<double> sg=0;
+//                 complex<double> t(0,1);
+//                 double G[3]={2*M_PI*kx/box[0][0], 2*M_PI*ky/box[1][1], 2*M_PI*kz/box[2][2]};
+//                 #pragma omp parallel for reduction(+: sg)
+//                     for (int  i = 0; i < natoms; i++){
+//                         double G_dot_r=G[0]*PosIons[i][0]+G[1]*PosIons[i][1]+G[2]*PosIons[i][2];
+//                         complex<double> charge(ion_charges[i],0.0);
+//                         sg+=charge*(cos(G_dot_r)+t*sin(G_dot_r));
+//                     }
+//                 double norm_sg = norm(sg);
+//                 double mod_g = sqrt(G[0]*G[0]+G[1]*G[1]+G[2]*G[2]);
+//                 reci_energy+=(1/pow(mod_g,2))*exp(-pow((mod_g)/(betaa*2),2))*norm_sg;
+//             }
+//         }
+//     }
+//     reci_energy*=(2*M_PI)/(box[0][0]*box[1][1]*box[2][2]);
+//     return reci_energy;
+// }
+
+//* synchronization construct critical
 double reci_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K){
     int nthreads;
     double reci_energy=0;
