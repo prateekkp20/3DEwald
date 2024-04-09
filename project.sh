60838f20c91adcb8c25d5a3185eeb423d1bd736a
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-04-09 23:19:41 +0530

    New Loop

--- project.sh
+++ project.sh
@@ -13,69 +13,98 @@
 
 ## code for multiple cells and same threads
 
-make clean > terminal.txt
-make >> terminal.txt
-
-cd run/
-echo "File,Reciprocal,," > parallel1.csv
-# echo "File,Self,,Reciprocal,,Real" > parallel1.csv
-
-for i in {1..14} 
-do
-    sed -i "s/Posfile = big\/POSCAR\.$((i-1))/Posfile = big\/POSCAR\.$i/g" input.in
-    echo -n "POSCAR"$i >> parallel1.csv
-    for j in {1..10}
-    do
-        ./coulomb.x >> parallel1.csv
-        echo " " >> parallel1.csv
-    done
-    echo " " >> parallel1.csv
-    echo " " >> parallel1.csv
-done
-
-
-
-## code for multiple different threads experiment
+# make clean > terminal.txt
+# make >> terminal.txt
 
 # cd run/
-# echo "# of Threads,Self,,Reciprocal,,Real" > original.csv
+# echo "File,Reciprocal,," > parallel1.csv
+# # echo "File,Self,,Reciprocal,,Real" > parallel1.csv
+
+# for i in {1..14} 
+# do
+#     sed -i "s/Posfile = big\/POSCAR\.$((i-1))/Posfile = big\/POSCAR\.$i/g" input.in
+#     echo -n "POSCAR"$i >> parallel1.csv
+#     for j in {1..10}
+#     do
+#         ./coulomb.x >> parallel1.csv
+#         echo " " >> parallel1.csv
+#     done
+#     echo " " >> parallel1.csv
+#     echo " " >> parallel1.csv
+# done
+
+
+
+## code for multiple threads experiment but same cell
+
+# cd run/
+# echo "# of Threads,Self,,Reciprocal,,Real" > original_poscar15_fifty.csv
 # cd ..
-# for i in {2..15}
+# for i in {2..24}
 # do
 #     sed -i "s/\#define NUM_THREADS $((i-1))/\#define NUM_THREADS $i/g" inc/const.h
 #     make clean > output.txt
 #     make >> output.txt
 
 #     cd run/
-#     echo -n $i >> original.csv
-#     for j in {1..10}
+#     echo -n $i >> original_poscar15_fifty.csv
+#     for j in {1..5}
 #     do
-#         ./coulomb.x >> original.csv
-#         echo " " >> original.csv
+#         ./coulomb.x >> original_poscar15_fifty.csv
+#         echo " " >> original_poscar15_fifty.csv
 #     done
-#     echo " " >> original.csv
-#     echo " " >> original.csv
+#     echo " " >> original_poscar15_fifty.csv
+#     echo " " >> original_poscar15_fifty.csv
 #     cd ..
 # done
 
 
-# code for bench marking the calculations using lammps
-
+# # code for bench marking the calculations using lammps
 # make clean > terminal.txt
 # make >> terminal.txt
 
 # cd run/
-# echo "File,Ecoul,Elong,Total" > output.csv
+# # echo "File,Ecoul,Elong,Total" > output.csv
 
-# for i in {1..14}
+# for i in {1..5}
 # do 
-#     sed -i "s/Posfile = big\/POSCAR\.$((i-1))/Posfile = big\/POSCAR\.$i/g" input.in
-#     echo -n "POSCAR"$i >> output.csv
-#     for j in {1..10}
-#     do 
-#         ./coulomb.x >> output.csv
-#         echo " " >> output.csv
-#     done
-#     echo " " >> output.csv
-#     echo " " >> output.csv
+#     sed -i "s/Posfile = bench\/benchfour$((i-1))\.POSCAR/Posfile = bench\/benchfour$i\.POSCAR/g" input.in
+#     # echo -n "POSCAR"$i >> output.csv
+#     # for j in {1..10}
+#     # do 
+#         ./coulomb.x >> output1.csv
+#         echo " " >> output1.csv
+#     # done
+#     # echo " " >> output1.csv
+#     # echo " " >> output1.csv
 # done
+
+## code for multiple threads for different settings with same size and same number of atoms
+
+for cell in {1..10}
+do
+    sed -i "s/Posfile = same_same\/POSCAR\.$((cell-1))/Posfile = same_same\/POSCAR\.$cell/g" run/input.in
+    cd run/
+    echo "# of Threads,,Reciprocal,,Real" > same_poscar$cell\file.csv
+    cd ..
+    for i in {2..25}
+    do
+        sed -i "s/\#define NUM_THREADS $((i-1))/\#define NUM_THREADS $i/g" inc/const.h
+        make clean > output.txt
+        make >> output.txt
+
+        cd run/
+        echo -n $i >> same_poscar$cell\file.csv
+        for j in {1..5}
+        do
+            ./coulomb.x >> same_poscar$cell\file.csv
+            echo " " >> same_poscar$cell\file.csv
+        done
+        echo " " >> same_poscar$cell\file.csv
+        echo " " >> same_poscar$cell\file.csv
+        cd ..
+    done
+done
+
+# fool=1
+# echo "hey" >> hey$fool\to.txt
