1f05fec41d2bbc71fe0b59c35aef2a01c9a21222
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-05-20 17:28:04 +0530

    Real FIle changed

--- src/real_e.C
+++ src/real_e.C
@@ -90,11 +90,11 @@
 //* For reduction construct
     double real_energy(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
         double real_energy=0;
-        omp_set_num_threads(NUM_THREADS);
-        // omp_set_num_threads(thread::hardware_concurrency());
-        #pragma omp parallel for schedule(runtime) reduction(+: real_energy)
+        // omp_set_num_threads(NUM_THREADS);
+        omp_set_num_threads(thread::hardware_concurrency());
+        #pragma omp parallel for simd schedule(runtime) reduction(+: real_energy)
             for (int i = 0; i < natoms; i++){
-                // #pragma omp SIMD
+                #pragma omp SIMD
                 for (int j = 0; j < i; j++){
                     if(i!=j){
                         double modR=dist(PosIons,i,j,box);
