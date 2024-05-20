6d891b067b9f04c8ad6cb1dc524f92d7337999f9
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-05-20 17:22:00 +0530

    Changes in Main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,3 @@
-//----------------------------------------------------------------
-// V. Agarwal 
-// Starting date: 21.06.19
-// Last Modified:
-// code for computing rates in a liquid
-//-----------------------------------------------------------------
-
 #include "libinclude.h" 
 #include "potdec.h"
 #include "mathdec.h"
@@ -14,13 +7,6 @@
 #include "complex"
 
 int main(int argc, char **argv){
-
-	// cout<<"\n******************************************************************"<<endl;
-	// cout<<"******************************************************************"<<endl;
-	// cout<<"      Code for adsorption/desorption near a sold surface          "<<endl;
-	// cout<<"                    Written by Vishal Agarwal                     "<<endl;
-	// cout<<"******************************************************************"<<endl;
-	// cout<<"******************************************************************"<<endl<<endl<<endl;
 
 	string filename;
 	string posfile;
@@ -275,7 +261,6 @@ int main(int argc, char **argv){
 
 	// print_carcoor(PosIons, natoms, boxcell,  n_atomtype, natoms_type, atomtype, 0, i,'w', "CONTCAR");
 	float a=5.42/boxcell[0][0];
-	// float a=0.2168;
 
 	// chrono::time_point<std::chrono::system_clock> start, end;
 	// start = chrono::system_clock::now();
@@ -296,27 +281,28 @@ int main(int argc, char **argv){
 	end1 = chrono::system_clock::now();
 	chrono::duration<double> elapsed_seconds1 = end1- start1;
     time_t end_time1 = std::chrono::system_clock::to_time_t(end1);
+	// cout<<fixed<<setprecision(8)<< "Elapsed time: " << elapsed_seconds1.count() << " sec\n\n";
 	// cout<<fixed<<setprecision(8)<<elapsed_seconds1.count()<<",";
 	cout<< "Elapsed time: " << elapsed_seconds1.count() << " sec\n";
 
 	// chrono::time_point<std::chrono::system_clock> start2, end2;
 	// start2 = chrono::system_clock::now();
 	// double realenergy=real_energy(PosIons, ion_charges, natoms, a, boxcell)*unitzer;
-	// cout<<fixed<<setprecision(5)<<realenergy<<",";
+	// // cout<<fixed<<setprecision(5)<<realenergy<<",";
 	// cout<<fixed<<setprecision(5)<<"Real Energy: "<<realenergy<<" Kcal/mol"<<"\n";
 	// end2 = chrono::system_clock::now();
 	// chrono::duration<double> elapsed_seconds2 = end2 - start2;
     // time_t end_time2 = std::chrono::system_clock::to_time_t(end2);
-	// cout<<fixed<<setprecision(8)<<elapsed_seconds2.count()<<",";
-	// cout<< "elapsed time: " << elapsed_seconds2.count() << " sec";
+	// // cout<<fixed<<setprecision(8)<<elapsed_seconds2.count()<<",";
+	// cout<< "elapsed time: " << elapsed_seconds2.count() << " sec\n\n";
 
-	chrono::time_point<std::chrono::system_clock> start2, end2;
-	start2 = chrono::system_clock::now();
-	double recienergy_bs=bspline(PosIons, ion_charges, natoms, a, boxcell,60,6,5)*unitzer;
+	chrono::time_point<std::chrono::system_clock> start3, end3;
+	start3 = chrono::system_clock::now();
+	double recienergy_bs=bspline(PosIons, ion_charges, natoms, a, boxcell,100,6,7)*unitzer;
 	cout<<fixed<<setprecision(5)<<"Reciprocal Energy FFTW: "<<recienergy_bs<<" Kcal/mol"<<"\n";
-	end2 = chrono::system_clock::now();
-	chrono::duration<double> elapsed_seconds2 = end2 - start2;
-    time_t end_time2 = std::chrono::system_clock::to_time_t(end2);
+	end3 = chrono::system_clock::now();
+	chrono::duration<double> elapsed_seconds2 = end3 - start3;
+    time_t end_time2 = std::chrono::system_clock::to_time_t(end3);
 	cout<< "Elapsed time: " << elapsed_seconds2.count() << " sec\n";
 
 /* using std::chrono::duration_cast; */
