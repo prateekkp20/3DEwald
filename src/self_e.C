02cca56b4c8c274cfa9495e32df15c63957511d1
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2025-01-30 18:59:31 +0530

    replaced float

--- src/self_e.C
+++ src/self_e.C
@@ -1,7 +1,7 @@
 #include "libinclude.h"
 #include "const.h"
 
-double selfe(int n_atomtype, int *natoms_type, float *chargs, float betaa){
+double selfe(int n_atomtype, int *natoms_type, float *chargs, double betaa){
     double self_energy=0;
     for (int i = 0; i < n_atomtype; i++){
         self_energy+=natoms_type[i]*pow(chargs[i],2);
