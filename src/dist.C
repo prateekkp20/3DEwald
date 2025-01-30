06ca6b5c4b0fa891e6273739794c21cfbebc1dc0
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2025-01-30 18:57:36 +0530

    less repetitive code

--- src/dist.C
+++ src/dist.C
@@ -1,10 +1,10 @@
-//***********************************************
+//**********************************************************************
 // function to calculate distances according to minimum image convention 
-//***********************************************
+//**********************************************************************
 
 #include "libinclude.h" 
 
-double dist(double **PosIons, int atom1, int atom2, float **box){
+double dist(double **PosIons, int atom1, int atom2, double **box){
 
 	double Dx, Dy, Dz;
 	double Dx1, Dy1, Dz1;
@@ -13,12 +13,14 @@ double dist(double **PosIons, int atom1, int atom2, float **box){
 	Dx = PosIons[atom1][0] - PosIons[atom2][0];
 	Dy = PosIons[atom1][1] - PosIons[atom2][1];
 	Dz = PosIons[atom1][2] - PosIons[atom2][2];
+
+	double a  = ceil(Dx/box[0][0]-0.5), b = ceil(Dy/box[1][1]-0.5), c=ceil(Dz/box[2][2]-0.5);
 	
-    Dx1 = Dx - box[0][0]*ceil(Dx/box[0][0]-0.5) - box[1][0]*ceil(Dy/box[1][1]-0.5) - box[2][0]*ceil(Dz/box[2][2]-0.5);
-	Dy1 = Dy - box[0][1]*ceil(Dx/box[0][0]-0.5) - box[1][1]*ceil(Dy/box[1][1]-0.5) - box[2][1]*ceil(Dz/box[2][2]-0.5);
-	Dz1 = Dz - box[0][2]*ceil(Dx/box[0][0]-0.5) - box[1][2]*ceil(Dy/box[1][1]-0.5) - box[2][2]*ceil(Dz/box[2][2]-0.5);
+    Dx1 = Dx - box[0][0]*a - box[1][0]*b - box[2][0]*c;
+    Dy1 = Dy - box[0][1]*a - box[1][1]*b - box[2][1]*c;
+    Dz1 = Dz - box[0][2]*a - box[1][2]*b - box[2][2]*c;
 	
-	distatoms = sqrt(pow(Dx1,2) + pow(Dy1,2) + pow(Dz1,2));
+	distatoms = sqrt(Dx1*Dx1 + Dy1*Dy1 + Dz1*Dz1);
 	return distatoms;
 	
 }
