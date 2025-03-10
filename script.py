26359116ead2e0b3d847c84ba492d1102fe8f52f
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2025-03-10 10:55:59 +0530

    Variation of errors and time of PPPM with changing parameters

--- script.py
+++ script.py
@@ -9,6 +9,18 @@ import csv
 import subprocess
 from subprocess import PIPE
 
+def ChargeFile(file_path, new_charges):
+    """
+    Replaces the charges in the given file with new charges.
+    
+    Args:
+        file_path (str): Path to the file to be modified.
+        new_charges (list): List of new charges to replace the existing ones.
+    """
+    with open(file_path, 'w') as file:
+        for charge in new_charges:
+            file.write(f"{charge}\n")
+
 def replace_line_in_file(file_path, start_words, new_line):
     """
     Replaces a line in the given file that starts with a specified phrase.
@@ -31,25 +43,79 @@ def replace_line_in_file(file_path, start_words, new_line):
     with open(file_path, 'w') as file:
         file.writelines(modified_lines)                 # Write modified content back to the file
 
-Input_File  = "input.in"
-CSV_File = "POSCAR_Files/Ewald3D/NaCl/out.csv"
-POSCAR_File = "POSCAR_Files/Ewald3D/NaCl/POSCAR."
-Total = 8
-
 os.system(f"clear && make clean && make")
 os.chdir("run/")
 
-# # Write the header to the CSV file in text mode
-# with open(CSV_File, 'w', newline='') as csvfile:
-#     csvwriter = csv.writer(csvfile)
-#     csvwriter.writerow(['Ecoul', 'Time (sec)', 'Elong', 'Time(sec)', 'Elong(pppm)', 'Time(sec)', 'Total'])
+ChargeFilePath = "charge.in" 
+Input_File  = "input.in"
+Ewald_File = "ewald.in"
 
-# Just run the Ewald3D files, with simple FFTW
+NaCl = [1,-1]
+ChargeFile(ChargeFilePath, NaCl)
+CSV_File = "Output_Final/Exp2/New_NACL.csv"
+POSCAR_File = "POSCAR_Files/Ewald3D/NaCl/POSCAR."
+Total = 101
+
+# # Just run the Ewald3D files, with simple FFTW
+# with open(CSV_File, 'wb') as file:
+
+#     for i in range(1,Total):
+#         print(f"Running File POSCAR."+str(i).zfill(3))
+#         replace_line_in_file(Input_File,"Posfile = ","Posfile = "+POSCAR_File+str(i).zfill(3))
+#         for j in range(0,10):
+#             result = subprocess.run(["./coulomb.x"], stdout=PIPE, stderr=PIPE)
+#             file.write(result.stdout)  # Write stdout in binary format
+#             file.write(result.stderr)  # Write stderr in binary format
+
+# CaCl2 = [2,-1]
+# ChargeFile(ChargeFilePath, CaCl2)
+# CSV_File = "POSCAR_Files/Ewald3D/CaCl2/out.csv"
+# POSCAR_File = "POSCAR_Files/Ewald3D/CaCl2/POSCAR."
+# Total = 101
+
+# # Just run the Ewald3D files, with simple FFTW
+# with open(CSV_File, 'wb') as file:
+
+#     for i in range(1,Total):
+#         print(f"Running File POSCAR."+str(i).zfill(3))
+#         replace_line_in_file(Input_File,"Posfile = ","Posfile = "+POSCAR_File+str(i).zfill(3))
+#         for j in range(0,10):
+#             result = subprocess.run(["./coulomb.x"], stdout=PIPE, stderr=PIPE)
+#             file.write(result.stdout)  # Write stdout in binary format
+#             file.write(result.stderr)  # Write stderr in binary format
+
+# Variation of errors and time of PPPM with changing parameters(mesh, order, kvector) with various systems
 with open(CSV_File, 'wb') as file:
+    for i in range(1,Total,8):
 
-    for i in range(1,Total):
         replace_line_in_file(Input_File,"Posfile = ","Posfile = "+POSCAR_File+str(i).zfill(3))
-        for j in range(0,5):
-            result = subprocess.run(["./coulomb.x"], stdout=PIPE, stderr=PIPE)
-            file.write(result.stdout)  # Write stdout in binary format
-            file.write(result.stderr)  # Write stderr in binary format
+
+        for gridx in range(16,128,16):
+            for gridy in range(16,128,16):
+                for gridz in range(16,128,16):
+
+                    replace_line_in_file(Ewald_File,"gx = ","gx = "+str(gridx))
+                    replace_line_in_file(Ewald_File,"gy = ","gy = "+str(gridy))
+                    replace_line_in_file(Ewald_File,"gz = ","gz = "+str(gridz))
+
+                    for orderx in range(2,12,2):
+                        for ordery in range(2,12,2):
+                            for orderz in range(2,12,2):
+
+                                replace_line_in_file(Ewald_File,"nx = ","nx = "+str(orderx))
+                                replace_line_in_file(Ewald_File,"ny = ","ny = "+str(ordery))
+                                replace_line_in_file(Ewald_File,"nz = ","nz = "+str(orderz))
+
+                                for kvecx in range(4,8,1):
+                                    for kvecy in range(4,8,1):
+                                        for kvecz in range(4,8,1):
+
+                                            replace_line_in_file(Ewald_File,"kx = ","kx = "+str(kvecx))
+                                            replace_line_in_file(Ewald_File,"ky = ","ky = "+str(kvecy))
+                                            replace_line_in_file(Ewald_File,"kz = ","kz = "+str(kvecz))
+
+                                            for timer in range(0,5):
+                                                result = subprocess.run(["./coulomb.x"], stdout=PIPE, stderr=PIPE)
+                                                file.write(result.stdout)  # Write stdout in binary format
+                                                file.write(result.stderr)  # Write stderr in binary format
+
